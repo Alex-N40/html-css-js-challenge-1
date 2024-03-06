e7c4fd67edb9f69f607e2a10212547aec0d6d465
Alex-N40 <alex_n40@proton.me>
2024-03-06 11:16:25 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//console.log("Hi!");
+var mainTitle = document.querySelector("h1");
+mainTitle.innerText = "Yay, JavaScript!!";
+console.log(mainTitle);
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "#FEF2F1";
+console.log(body);
